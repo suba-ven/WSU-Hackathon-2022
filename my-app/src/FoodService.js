45dff9e910ec072c478c47b255fe16c25fd59f30
Subashree Venkatasubramanian <s.venkatasubramani2@wsu.edu>
2022-02-20 06:42:17 -0800

    display for dining hall

--- my-app/src/FoodService.js
+++ my-app/src/FoodService.js
@@ -35,7 +35,6 @@ class FoodService extends Building{
     //removePerson() {
         
     //        this.currentMap.delete(killPerson().getid());
-            
         
     //}
     pushPerson() {
@@ -73,13 +72,7 @@ class FoodService extends Building{
      *               <h1> {this.props.name}</h1>  
 
      *                 {this.shouldKill && <Button/>}
-
-     if(computerTime != date.getMinute())
-     {
-       re-render();
-       computerTime = date.getMinute();
-     }
-     
+     *                 
      //  <h2>Current time: {this.props.hours}:{this.props.minutes}:{this.props.seconds}
                 Current time ms: {this.props.milli}</h2>
      */
