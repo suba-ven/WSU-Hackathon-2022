ed8a2f6335180b99757293b8e11fb20213293b9f
Connor Lee <connor.lee1@wsu.edu>
2022-02-20 04:59:52 -0800

    stuff

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,11 +17,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
 //        <Building name="hello" address="123" maxOccupancy='12' organization = "WSU" currentOccupancy = '12' percentCapacity ='12'/>
 
 //         <FoodService hours={index.getHours()} minutes={index.getMinutes()} seconds={index.getSeconds()} milli={index.getTime()} />
